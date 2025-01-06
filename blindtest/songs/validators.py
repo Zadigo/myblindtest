3b7fbd60605d9549da7a8fe05f0fabd90e7b02d5
Zadigo <pendenquejohn@gmail.com>
2025-01-06 17:30:14 +0100

    Code cleaning

--- blindtest/songs/validators.py
+++ blindtest/songs/validators.py
@@ -15,11 +15,6 @@ def validate_year(value):
             raise ValidationError('Year is not valid')
 
 
-def validate_youtube(value):
-    validator = RegexValidator(r'youtube\.com\/embed\/')
-    validator(value)
-
-
 def validate_difficulty(value):
     if value < 1 or value > 5:
         raise ValidationError(
