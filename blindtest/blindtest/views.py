5c379814865d875b961bc36c9050a404f826f395
Zadigo <pendenquejohn@gmail.com>
2025-02-10 20:56:35 +0100

    Pass statistics to index

--- blindtest/blindtest/views.py
+++ blindtest/blindtest/views.py
@@ -1,8 +1,18 @@
 from django.views.generic import TemplateView
 from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
+from songs.models import Song
+from django.db.models.aggregates import Count
 
 
+@method_decorator(cache_page(1 * 60), name='dispatch')
 class HomePage(TemplateView):
     template_name = 'home.html'
-    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        statistics = Song.objects.aggregate(
+            song_count=Count('id')
+        )
+        context['statistics'] = statistics
+        return context
