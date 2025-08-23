a9a96f2cb35657b5d927224152bf15f3cb090e1a
Zadigo <pendenquejohn@gmail.com>
2025-08-24 00:49:56 +0200

    Improve managers

--- blindtest/songs/game_logic/managers.py
+++ blindtest/songs/game_logic/managers.py
@@ -1,9 +1,37 @@
-import dataclass
+import dataclasses
+import datetime
+from dataclasses import field
+
+import pytz
 
 
-@dataclass
+@dataclasses.dataclass
 class Device:
+    """Dataclass representing a device. A device
+    can be either a projector (projecting screen) or
+    a buzzer (smartphone used by the user to buzz)
+    in order to give an answer to a song to guess"""
+
     device_id: str
+    confirmed: bool = False
+    is_projector: bool = False
+    is_buzzer: bool = False
+    created_on: datetime.datetime = field(
+        default_factory=datetime.datetime.now)
+
+    def __post_init__(self):
+        self.created_on = self.created_on.replace(tzinfo=pytz.UTC)
+
+    def __repr__(self):
+        return f'Device(id={self.device_id}, confirmed={self.confirmed})'
+
+    def __eq__(self, other):
+        if not isinstance(other, Device):
+            return NotImplemented
+        return self.device_id == other.device_id
+
+    def __hash__(self):
+        return hash(self.device_id)
 
 
 class DeviceManager:
@@ -11,16 +39,11 @@ class DeviceManager:
     devices in the game e.g. projection devices"""
 
     def __init__(self):
-        self.pending_devices: dict[str, set[str]] = {
-            'projectors': set(),
-            'buzzers': set()
-        }
-        self.confirmed_devices: dict[str, set[str]] = {
-            'projectors': set(),
-            'buzzers': set()
-        }
+        self.devices: set[Device] = set()
+        self.connection_token: str | None = None
 
     def __get__(self, instance, cls):
+        self.connection_token = getattr(instance, 'connection_token', None)
         return self
 
     def __len__(self):
@@ -30,59 +53,53 @@ class DeviceManager:
         return iter(self.devices)
 
     def __getitem__(self, device_id: str):
-        return filter(lambda x: x == device_id, self.devices)
+        return list(filter(lambda x: x.device_id == device_id, self.devices))
+
+    def __repr__(self):
+        return f'DeviceManager(devices={list(self.devices)})'
 
     @property
     def pending_devices(self):
         """Devices that are pending confirmation"""
-        return self.pending_devices['projectors'].union(self.pending_devices['buzzers'])
+        return filter(lambda x: not x.confirmed, self.devices)
 
     @property
     def confirmed_devices(self):
         """Devices that have been confirmed via a connection token"""
-        return self.confirmed_devices['projectors'].union(self.confirmed_devices['buzzers'])
-
-    @property
-    def devices(self):
-        """All devices that are either pending or confirmed"""
-        return self.pending_devices.union(self.confirmed_devices)
+        return filter(lambda x: x.confirmed, self.devices)
 
     def add_pending_projector(self, device_id: str):
         """Adds this device to the list of pending projectors when
         the device is first connected and no connection token has been 
         received or validated by the admin device"""
-        if device_id in self.pending_devices:
+        if device_id in self.devices:
             return False
 
-        self.pending_devices['projectors'].add(device_id)
-        return True
+        self.devices.add(Device(device_id=device_id, is_projector=True))
 
     def add_pending_buzzer(self, device_id: str):
         """Adds this device to the list of pending buzzers when
         the device is first connected and no connection token has been
         received or validated by the admin device"""
-        if device_id in self.pending_devices:
+        if device_id in self.devices:
             return False
 
-        self.pending_devices['buzzers'].add(device_id)
+        self.devices.add(Device(device_id=device_id, is_buzzer=True))
         return True
 
-    def add_confirmed_projector(self, device_id: str):
-        """Adds this device to the confirmed list of projectors
+    def confirm_device(self, device_id: str):
+        """Adds a pending device to the confirmed list of devices
         if the connection token has been accepted by the admin device"""
-        if device_id in self.confirmed_devices:
-            return False
+        for device in self.devices:
+            if device.device_id == device_id and not device.confirmed:
+                device.confirmed = True
+                return True
+        return False
 
-        self.pending_devices['projectors'].discard(device_id)
-        self.confirmed_devices['projectors'].add(device_id)
-        return True
-
-    def add_confirmed_buzzer(self, device_id: str):
-        """Adds this device to the confirmed list of buzzers
-        if the connection token has been accepted by the admin device"""
-        if device_id in self.confirmed_devices:
-            return False
-
-        self.pending_devices['buzzers'].discard(device_id)
-        self.confirmed_devices['buzzers'].add(device_id)
-        return True
+    def remove(self, device_id: str):
+        """Removes a device from the list of devices"""
+        for device in self.devices:
+            if device.device_id == device_id:
+                self.devices.remove(device)
+                return True
+        return False
