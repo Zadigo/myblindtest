995dcb7836be11c8a4bdc29083d187d47e8f3d97
Zadigo <pendenquejohn@gmail.com>
2025-08-24 01:08:53 +0200

    Refactor consumer

--- blindtest/songs/consumers.py
+++ blindtest/songs/consumers.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Any, Union
 
 import pyotp
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
@@ -91,7 +91,11 @@ class SongConsumer(GameLogicMixin, ChannelEventsMixin, AsyncJsonWebsocketConsume
     async def disconnect(self, code):
         # TODO: Channel make diffusion group
         message = self.base_room_message(**{'type': 'game.disconnected'})
+
+        await self.channel_layer.group_send(self.waiting_room_name, message)
         await self.channel_layer.group_send(self.diffusion_group_name, message)
+
+        await self.channel_layer.group_discard(self.waiting_room_name, self.channel_name)
         await self.channel_layer.group_discard(self.diffusion_group_name, self.channel_name)
 
         if self.timer_task:
@@ -191,9 +195,10 @@ class SongConsumer(GameLogicMixin, ChannelEventsMixin, AsyncJsonWebsocketConsume
                 message = {'action': 'song_skipped', 'song': self.current_song}
                 await self.send_json(message)
 
-                group_message = self.base_room_message(**{'type': 'game.updates', 'message': message})
+                group_message = self.base_room_message(
+                    **{'type': 'game.updates', 'message': message})
                 await self.channel_layer.group_send(self.diffusion_group_name, group_message)
-                
+
                 await self.next_song()
         elif action == 'randomize_genre':
             # Select a temporary genre within songs, if and only if
@@ -352,4 +357,39 @@ class SmartphoneConsumer(ChannelEventsMixin, AsyncJsonWebsocketConsumer):
     """This consumer handles connections specifically from smartphone devices
     which can then be used to interact with the game. The smartphones are
     used as buzzers. When the user buzzes, it stops the game and the timer which
-    then allows the admin to determine if the answer is correct or incorrect."""
+    then allows the admin to determine if the answer is correct or incorrect"""
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+
+    #     self.device_name = 'projection_screen'
+    #     self.device_id = f'buzz_{get_random_string(length=20)}'
+    #     # The admin device that accepted the code
+    #     # and to which this device is linked to
+    #     self.admin_device: str | None = None
+
+    # async def connect(self):
+    #     await super().connect()
+
+    # async def receive_json(self, content: dict[str, str | dict[str, Any]], **kwargs):
+    #     action = content.get('action')
+
+    #     if action is None:
+    #         await self.send_error('No action was provided')
+    #         return
+
+    #     if action == 'idle_connect':
+    #         # Subscribe to the waiting room
+    #         await self.channel_layer.group_add(self.waiting_room_name, self.channel_name)
+
+    #         # Notify the waiting room that a device has connected. There can be instances
+    #         # where the room does not exist (it is created by the admin device)
+    #         message = self.base_room_message(
+    #             **{
+    #                 'type': 'device.connected',
+    #                 'thread': 'waiting_room'
+    #             }
+    #         )
+
+    #         await self.channel_layer.group_send(self.waiting_room_name, message)
+    #         await self.send_json({'action': 'idle_connect', 'message': self.device_id})
