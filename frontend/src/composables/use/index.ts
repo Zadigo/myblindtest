6748a33d60dcd23140a213c8541e40991bacda39
Zadigo <pendenquejohn@gmail.com>
2025-11-24 21:25:34 +0100

    Clean composable codes

--- frontend/src/composables/use/index.ts
+++ frontend/src/composables/use/index.ts
@@ -7,38 +7,44 @@ export * from './session'
 export * from './utils'
 
 /**
- * Loads autocomplete data for the minimum and maximum time
- * period for the songs, the count by genre
- * 
+ * Loads autocomplete data for some input fields
  * @param fromCache Whether to load data from cache or fetch from API
  */
 export const useLoadAutocompleteData = createSharedComposable((fromCache = false) => {
   const autocomplete = ref<SettingsApiResponse | undefined>()
 
+  // TODO: Memoize does not work properly
   const { load } = useMemoize(async (path: string) => {
     const { responseData } = await useAsyncRequest<SettingsApiResponse>('django', path, { immediate: true })
     return responseData.value
   })
 
-  onMounted(async () => { autocomplete.value = await load('/api/v1/songs/settings') })
+  tryOnMounted(async () => {
+    autocomplete.value = await load('/api/v1/songs/settings')
+  })
 
   const minimumPeriod = computed(() => autocomplete.value?.period.minimum || 0)
   const maximumPeriod = computed(() => autocomplete.value?.period.maximum || 100)
+
   const genreDistribution = computed<GenreDistribution[]>(() => autocomplete.value?.count_by_genre || [])
+  
   const genres = useArrayMap(genreDistribution, (item) => ({ label: item.genre, name: item.genre }))
   const genreNames = useArrayMap(genreDistribution, (item) => item.genre)
 
   return {
     /**
      * Autocomplete data for the songs settings
+     * @default undefined
      */
     autocomplete,
     /**
      * Minimum period for the songs
+     * @default 0
      */
     minimumPeriod,
     /**
      * Maximum period for the songs
+     * @default 100
      */
     maximumPeriod,
     /**
@@ -53,18 +59,5 @@ export const useLoadAutocompleteData = createSharedComposable((fromCache = false
      * List of genre names for the songs
      */
     genreNames
-  }
-})
-
-/**
- * Composable to manage dark mode state
- */
-export const useDarkMode = createSharedComposable(() => {
-  const isDark = useDark({ selector: 'html', attribute: 'class', valueDark: 'p-dark',})
-  const toggleDark = useToggle(isDark)
-
-  return {
-    isDark,
-    toggleDark
   }
 })
--- frontend/src/composables/use/utils.ts
+++ frontend/src/composables/use/utils.ts
@@ -1,13 +1,19 @@
+import type { Arrayable } from '@/types'
+
+/**
+ * Adds string functionnalities like pluralization
+ */
 export function useString() {
-  function plural(items: (string | number | object)[], word: string) {
-    if (items.length === 0 || items.length > 1) {
-      return `${word}s`
-    } else {
-      return word
-    }
+  function plural(items: Arrayable<string | number | object>, word: string) {
+    return (Array.isArray(items) ? items.length : items) > 1 ? `${word}s` : word
   }
 
   return {
+    /**
+     * Pluralizes a word based on the number of items
+     * @param items The items to count
+     * @param word The word to pluralize
+     */
     plural
   }
 }
@@ -21,9 +27,43 @@ export const useGlobalState = createGlobalState(() => {
   const [showConnectionUrl, toggleShowConnectionUrl] = useToggle(false)
 
   return {
+    /**
+     * Whether to show the About modal
+     * @default false
+     */
     showAbout,
-    toggleShowAbout,
+    /**
+     * Whether to show the Connection URL modal
+     * @default false
+     */
     showConnectionUrl,
+    /**
+     * Toggles the About modal visibility
+     */
+    toggleShowAbout,
+    /**
+     * Toggles the Connection URL modal visibility
+     */
     toggleShowConnectionUrl
+  }
+})
+
+/**
+ * Composable to manage dark mode state
+ */
+export const useDarkMode = createSharedComposable(() => {
+  const isDark = useDark({ selector: 'html', attribute: 'class', valueDark: 'p-dark', })
+  const toggleDark = useToggle(isDark)
+
+  return {
+    /**
+     * Whether dark mode is enabled
+     * @default false
+     */
+    isDark,
+    /**
+     * Toggles dark mode
+     */
+    toggleDark
   }
 })
